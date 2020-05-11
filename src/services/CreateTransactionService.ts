f9a836d656ec1d054516281d84ea937eefa140b3
Rafael V. dos Santos <rvsantos00@gmail.com>
2020-05-11 16:59:42 -0300

    ensure to find or create category if there is no

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,8 +1,9 @@
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, getRepository } from 'typeorm';
 // import AppError from '../errors/AppError';
 
 import TransactionRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 
 interface IRequest {
   title: string;
@@ -20,15 +21,33 @@ class CreateTransactionService {
   }: IRequest): Promise<Transaction> {
     const transactionsRepository = getCustomRepository(TransactionRepository);
 
+    const transactionCategory = await this.findOrCreateCategory(category);
+
     const transaction = transactionsRepository.create({
       title,
       value,
       type,
+      category: transactionCategory,
     });
 
     await transactionsRepository.save(transaction);
 
     return transaction;
+  }
+
+  private async findOrCreateCategory(title: string): Promise<Category> {
+    const categoryRepository = getRepository(Category);
+
+    let category = await categoryRepository.findOne({
+      where: { title },
+    });
+
+    if (category) return category;
+
+    category = categoryRepository.create({ title });
+    await categoryRepository.save(category);
+
+    return category;
   }
 }
 
