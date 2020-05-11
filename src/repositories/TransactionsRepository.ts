01695e313a08f3b36aba18d8a0eb1819d56fb157
Rafael V. dos Santos <rvsantos00@gmail.com>
2020-05-11 15:53:56 -0300

    ensure the ability to list transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -2,7 +2,7 @@ import { EntityRepository, Repository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
 
-interface Balance {
+interface IBalance {
   income: number;
   outcome: number;
   total: number;
@@ -10,9 +10,25 @@ interface Balance {
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
-  // public async getBalance(): Promise<Balance> {
-  //   // TODO
-  // }
+  public async getBalance(): Promise<IBalance> {
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce(
+      (accumulator, transaction) => {
+        accumulator[transaction.type] += Number(transaction.value);
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
+  }
 }
 
 export default TransactionsRepository;
