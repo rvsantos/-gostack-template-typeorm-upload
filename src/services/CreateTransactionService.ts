153223ed21302f916fbb2db661c11e27d0a06b88
Rafael V. dos Santos <rvsantos00@gmail.com>
2020-05-11 14:53:13 -0300

    ensure the ability to create a new transaction

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,34 @@
+import { getCustomRepository } from 'typeorm';
 // import AppError from '../errors/AppError';
 
+import TransactionRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface IRequest {
+  title: string;
+  value: number;
+  category: string;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    category,
+    type,
+  }: IRequest): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionRepository);
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
